33621182498220c5dd7523b3b1e3930253d921e6
jungeunlee95 <leeap1004@gmail.com>
2019-12-04 18:11:52 +0900

    modify gradle settings

--- src/test/java/com/test/hello/DockerTestApplicationTests.java
+++ src/test/java/com/test/hello/DockerTestApplicationTests.java
@@ -1,13 +1,13 @@
-package com.test.hello;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class DockerTestApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.test.hello;
+//
+//import org.junit.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class DockerTestApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
