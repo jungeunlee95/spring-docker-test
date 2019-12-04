b334b1e04af9161b553e4afe8448aa2532cfb29d
jungeunlee95 <leeap1004@gmail.com>
2019-12-04 22:19:50 +0900

    add path

---
+++ src/main/java/com/test/hello/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.test.hello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController{
+
+    @GetMapping("/hello")
+    public String createApp()  {
+        return "hello! docker test";
+    }
+
+}
